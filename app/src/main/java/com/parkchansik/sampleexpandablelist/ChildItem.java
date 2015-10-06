2cbdbdd7d2eb610df2bf6756c291da7402b615b5
Park chansik <cksltr@naver.com>
2015-10-06 14:04:38 +0900

    Initial commit

---
+++ app/src/main/java/com/parkchansik/sampleexpandablelist/ChildItem.java
@@ -0,0 +1,8 @@
+package com.parkchansik.sampleexpandablelist;
+
+/**
+ * Created by dongja94 on 2015-10-06.
+ */
+public class ChildItem {
+    String child;
+}
---
+++ app/src/main/java/com/parkchansik/sampleexpandablelist/GroupItem.java
@@ -0,0 +1,13 @@
+package com.parkchansik.sampleexpandablelist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dongja94 on 2015-10-06.
+ */
+public class GroupItem {
+    String groupName;
+    // ....
+    List<ChildItem> children = new ArrayList<ChildItem>();
+}
